5368740a06084589ee0b41524fa716ca463cf11d
sonyokpara <sony.okpara@yahoo.com>
2024-06-09 13:45:32 +0000

    Complete the top nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,79 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import logo from "../assets/logo.png";
+import { navItems } from "../constants";
 
 const Navbar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenuOpen = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
   return (
     <header className="sticky top-0 z-50 backdrop-blur-lg py-3 border-b border-neutral-700/80">
       <nav className="px-4 mx-auto relative text-ms container">
-        <div className="flex justify-center items-center">
+        <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img src={logo} alt="logo" className="w-10 h-10 mr-2" />
             <span className="tracking-tight text-xl">VirtualR</span>
           </div>
+
+          <ul className="hidden lg:flex space-x-12 ml-12">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+
+          <div className="hidden lg:flex justify-center items-center space-x-8">
+            <a
+              href="#"
+              className="px-2 py-1 border border-slate-500/55 rounded-md"
+            >
+              Sign In
+            </a>
+            <a
+              href="#"
+              className="bg-gradient-to-r from-orange-500 to-orange-800 py-1 px-2 rounded-md"
+            >
+              Create an account
+            </a>
+          </div>
+          <div className="lg:hidden md:flex justify-end py-1 px-2">
+            <button onClick={toggleMobileMenuOpen}>
+              {mobileMenuOpen ? <X /> : <Menu />}
+            </button>
+          </div>
         </div>
+        {mobileMenuOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 lg:hidden flex flex-col justify-center items-center p-12 w-full">
+            <ul className="space-y-8">
+              {navItems.map((item, index) => (
+                <li key={index}>
+                  <a href={item.href} className="hover:text-gray-400">
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-8 mt-10">
+              <a
+                href="#"
+                className="px-2 py-1 border border-slate-500/55 rounded-md"
+              >
+                Sign In
+              </a>
+              <a
+                href="#"
+                className="bg-gradient-to-r from-orange-500 to-orange-800 py-1 px-2 rounded-md"
+              >
+                Create an account
+              </a>
+            </div>
+          </div>
+        )}
       </nav>
     </header>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -60,37 +60,37 @@ export const testimonials = [
 
 export const features = [
   {
-    icon: <BotMessageSquare />,
+    icon: BotMessageSquare,
     text: "Drag-and-Drop Interface",
     description:
       "Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.",
   },
   {
-    icon: <Fingerprint />,
+    icon: Fingerprint,
     text: "Multi-Platform Compatibility",
     description:
       "Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.",
   },
   {
-    icon: <ShieldHalf />,
+    icon: ShieldHalf,
     text: "Built-in Templates",
     description:
       "Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.",
   },
   {
-    icon: <BatteryCharging />,
+    icon: BatteryCharging,
     text: "Real-Time Preview",
     description:
       "Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.",
   },
   {
-    icon: <PlugZap />,
+    icon: PlugZap,
     text: "Collaboration Tools",
     description:
       "Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.",
   },
   {
-    icon: <GlobeLock />,
+    icon: GlobeLock,
     text: "Analytics Dashboard",
     description:
       "Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.",
